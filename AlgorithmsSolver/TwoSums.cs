8b514f22b66273b5270aea117dd5edb3850a3759
Евгений <@DESKTOP-7F30QJL>
2024-07-15 21:30:56 +0500

    Добавьте файлы проекта.

---
+++ AlgorithmsSolver/TwoSums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsSolver
+{
+    internal class TwoSums
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            HashSet<int> result = new();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var snd = target - nums[i];
+                if (result.Contains(snd))
+                {
+                    return;
+                }
+                else
+                {
+                    result.Add(nums[i]);
+                }
+            }
+        }
+    }
+}
