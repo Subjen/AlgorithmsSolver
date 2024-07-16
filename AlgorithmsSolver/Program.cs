09b38b885d83b71f833554eb2fed036e6cc450f1
subjen <subbotinjenay@mail.ru>
2024-07-16 21:17:05 +0500

    Add RemoveDuplicates Solve https://leetcode.com/problems/two-sum/description/

--- AlgorithmsSolver/Program.cs
+++ AlgorithmsSolver/Program.cs
@@ -1,13 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AlgorithmsSolver;
 
-Console.WriteLine("Hello, World!");
-var ts = new TwoSums();
-var ts1 = ts.TwoSum([2, 7, 11, 15], 9);
-Console.WriteLine("{0} {1}", ts1[0], ts1[1]);
+//Console.WriteLine("Hello, World!");
+//var ts = new TwoSums();
+//var ts1 = ts.TwoSum([2, 7, 11, 15], 9);
+//Console.WriteLine("{0} {1}", ts1[0], ts1[1]);
 
-var ts2 = ts.TwoSum([3, 2, 4], 6);
-Console.WriteLine("{0} {1}", ts2[0], ts2[1]);
+//var ts2 = ts.TwoSum([3, 2, 4], 6);
+//Console.WriteLine("{0} {1}", ts2[0], ts2[1]);
 
-var ts3 = ts.TwoSum([3, 3], 6);
-Console.WriteLine("{0} {1}", ts3[0], ts3[1]);
+//var ts3 = ts.TwoSum([3, 3], 6);
+//Console.WriteLine("{0} {1}", ts3[0], ts3[1]);
+
+
+var rds = new RemoveDuplicatesSolver();
+var nums = new[] { 1, 1, 2 };
+var rds1 = rds.RemoveDuplicates(nums);
+Console.WriteLine(rds1);
+Console.WriteLine(string.Join(" ", nums));
+
+
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+var rds2 = rds.RemoveDuplicates(nums);
+Console.WriteLine(rds2);
+Console.WriteLine(string.Join(" ", nums));
---
+++ AlgorithmsSolver/RemoveDuplicatesSolver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsSolver
+{
+    internal class RemoveDuplicatesSolver
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int diff = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] == nums[i - 1])
+                {
+                    diff++;
+                }
+                nums[i - diff] = nums[i];
+            }
+            return nums.Length - diff;
+        }
+    }
+}
