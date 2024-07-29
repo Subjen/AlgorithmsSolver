603f330b850d203fd6ac1fea09c16aa8859c67f1
subjen <subbotinjenay@mail.ru>
2024-07-29 20:16:00 +0500

    Add BaseballGame Solve https://leetcode.com/problems/baseball-game/submissions/1337402569/

---
+++ AlgorithmsSolver/Stack/BaseballGameSolver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsSolver.Stack
+{
+    internal class BaseballGameSolver
+    {
+        public int CalPoints(string[] operations)
+        {
+            Stack<int> Numbers = new();
+
+            foreach (string op in operations)
+            {
+                if (op == "+")
+                {
+                    var a = Numbers.Pop();
+                    var b = Numbers.Peek();
+                    Numbers.Push(a);
+                    Numbers.Push(a + b);
+                }
+                else if (op == "C")
+                {
+                    Numbers.Pop();
+                }
+                else if (op == "D")
+                { 
+                    var n = Numbers.Peek();
+                    Numbers.Push(n * 2);
+                }
+                else
+                {
+                    var num = int.Parse(op);
+                    Numbers.Push(num);
+                }
+            }
+
+            return Numbers.Sum();
+
+        }
+
+    }
+}
