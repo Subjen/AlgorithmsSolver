9770131f86d75c4c91aa0add7967362a326c4765
subjen <subbotinjenay@mail.ru>
2024-08-04 15:48:12 +0500

    Add 496. Next Greater Element I solver https://leetcode.com/problems/next-greater-element-i/submissions/1344141171/

---
+++ AlgorithmsSolver/Stack/NextGreaterElement1Solver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsSolver.Stack
+{
+    internal class NextGreaterElement1Solver
+    {
+        public int[] NextGreaterElement(int[] nums1, int[] nums2)
+        {
+            var decreasingStack = new Stack<int>();
+            var map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums2.Length; i++)
+            {
+                var currNum = nums2[i];
+                map[currNum] = -1;
+                while (decreasingStack.Count > 0 && decreasingStack.Peek() < currNum)
+                {
+                    var num = decreasingStack.Pop();
+                    map[num] = currNum;
+                }
+                decreasingStack.Push(currNum);
+            }
+
+            return nums1.Select(num => map[num]).ToArray();
+
+        }
+    }
+}
