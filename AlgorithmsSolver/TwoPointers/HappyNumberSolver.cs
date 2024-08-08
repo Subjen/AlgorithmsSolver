a13c7cd9637df57db7807b6b9e0e16f517d4aad9
subjen <subbotinjenay@mail.ru>
2024-08-08 20:51:13 +0500

    Add another solution of problem 202. Happy Number https://leetcode.com/problems/happy-number/submissions/1349031840/

--- AlgorithmsSolver/TwoPointers/HappyNumberSolver.cs
+++ AlgorithmsSolver/TwoPointers/HappyNumberSolver.cs
@@ -8,35 +8,35 @@ namespace AlgorithmsSolver.TwoPointers
 {
     internal class HappyNumberSolver
     {
-        public bool IsHappy(int n)
+        Dictionary<int, int> square = new()
         {
-            var square = new Dictionary<int, int>()
-            {
-                [0] = 0,
-                [1] = 1,
-                [2] = 4,
-                [3] = 9,
-                [4] = 16,
-                [5] = 25,
-                [6] = 36,
-                [7] = 49,
-                [8] = 64,
-                [9] = 81,
-            };
+            [0] = 0,
+            [1] = 1,
+            [2] = 4,
+            [3] = 9,
+            [4] = 16,
+            [5] = 25,
+            [6] = 36,
+            [7] = 49,
+            [8] = 64,
+            [9] = 81,
+        };
 
-            var sums = new HashSet<int>();
+        HashSet<int> sums = new();
 
-            int GetSumOfSquare(int n)
+        int GetSumOfSquare(int n)
+        {
+            var sum = 0;
+            while (n > 0)
             {
-                var sum = 0;
-                while (n > 0)
-                {
-                    sum += square[n % 10];
-                    n /= 10;
-                }
-                return sum;
+                sum += square[n % 10];
+                n /= 10;
             }
+            return sum;
+        }
 
+        public bool IsHappy2(int n)
+        {
             while (n != 1)
             {
                 n = GetSumOfSquare(n);
@@ -45,6 +45,25 @@ namespace AlgorithmsSolver.TwoPointers
             }
 
             return n == 1;
+        }
+
+        // two pointers algorithm
+        // Floyd Cycle Detection (the one we used in Detect Linked List Cycle)
+        // don't have to use hashtable
+        public bool IsHappy(int n)
+        {
+            int slow, fast;
+            slow = fast = n;
+            do
+            {
+                slow = GetSumOfSquare(slow);
+                fast = GetSumOfSquare(fast);
+                fast = GetSumOfSquare(fast);
+                if (fast == 1) 
+                    return true;
+            } 
+            while (slow != fast);
+            return false;
         }
     }
 }
