b4ea0ce16bcc74a941b5984765fb3f821fdd19c4
subbotinea@suek.ru <subbotinea@suek.ru>
2024-11-20 11:17:37 +0300

    94. Binary Tree Inorder Traversal https://leetcode.com/problems/binary-tree-inorder-traversal/submissions/1457916222/

---
+++ AlgorithmsSolver/TreeNode/Binary/BinaryTreeInorderTraversalSolver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsSolver.TreeNode.Binary
+{
+    //https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+    internal class BinaryTreeInorderTraversalSolver
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            var nodes = new List<int>();
+            void DFS(TreeNode node, List<int> nodes)
+            {
+                if (node == null)
+                    return;
+
+                if (node.left != null)
+                {
+                    DFS(node.left, nodes);
+                }
+                nodes.Add(node.val);
+                if (node.right != null)
+                {
+                    DFS(node.right, nodes);
+                }
+            }
+            DFS(root, nodes);
+            return nodes;
+        }
+    }
+}
