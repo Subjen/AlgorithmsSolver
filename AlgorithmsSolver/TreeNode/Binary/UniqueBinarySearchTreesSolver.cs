d446a2176aa2c8df53d5ddec1f3ba45b3dd41401
subbotinea@suek.ru <subbotinea@suek.ru>
2024-11-20 11:18:06 +0300

    96. Unique Binary Search Trees https://leetcode.com/problems/unique-binary-search-trees/submissions/1457954166/

---
+++ AlgorithmsSolver/TreeNode/Binary/UniqueBinarySearchTreesSolver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsSolver.TreeNode.Binary
+{
+    //https://leetcode.com/problems/unique-binary-search-trees/description/
+    internal class UniqueBinarySearchTreesSolver
+    {
+        // ищем комбинацию предыдущих решений 
+        public int NumTrees(int n)
+        {
+            var res = new int[n + 1];
+            res[0] = 1;
+            res[1] = 1;
+
+            for (int i = 2; i < n + 1; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    res[i] += res[j] * res[i - j - 1]; 
+                }
+            }
+
+            return res[n];
+        }
+
+    }
+}
