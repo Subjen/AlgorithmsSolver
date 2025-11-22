3a2ce116b39cbc974d40284386a624e5081873c2
subjen <subbotinjenay@mail.ru>
2025-11-22 16:17:52 +0500

    Solve 11. Container With Most Water https://leetcode.com/problems/container-with-most-water/submissions/1836674536/

---
+++ AlgorithmsSolver/TwoPointers/ContainerWithMostWaterSolver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsSolver.TwoPointers
+{
+    internal class ContainerWithMostWaterSolver
+    {
+        //https://leetcode.com/problems/container-with-most-water/description/
+        public int MaxArea(int[] height)
+        {
+            int left = 0, right = height.Length - 1;
+
+            var maxArea = 0;
+            while (left < right)
+            {
+                var currLength = right - left;
+
+                var minHeght = Math.Min(height[left], height[right]);
+                maxArea = Math.Max(maxArea, currLength * minHeght);
+
+                if (height[left] > height[right])
+                {
+                    var currRight = height[right];
+                    while (left < right && height[right] <= currRight)
+                    {
+                        right--;
+                    }
+                }
+                else
+                {
+                    var currLeft = height[left];
+                    while (left < right && height[left] <= currLeft)
+                    {
+                        left++;
+                    }
+                }
+            }
+
+            return maxArea;
+        }
+    }
+}
