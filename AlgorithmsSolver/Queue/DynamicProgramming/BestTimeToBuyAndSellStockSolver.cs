73c4ba80fefa0a9ec5837159e307f3e244c86bfb
subjen <subbotinjenay@mail.ru>
2025-11-26 18:50:36 +0500

    Solve 121. Best Time to Buy and Sell Stock
    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/1840272380/

---
+++ AlgorithmsSolver/Queue/DynamicProgramming/BestTimeToBuyAndSellStockSolver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsSolver.Queue.DynamicProgramming
+{
+    internal class BestTimeToBuyAndSellStockSolver
+    {
+        //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+        public int MaxProfit(int[] prices)
+        {
+            var max = 0; var min = prices[0];
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                max = Math.Max(max, prices[i] - min);
+
+                min = Math.Min(min, prices[i]);
+            }
+            return max;
+        }
+    }
+}
