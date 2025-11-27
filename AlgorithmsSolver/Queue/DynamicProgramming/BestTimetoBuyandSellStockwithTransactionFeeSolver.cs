06ec42184a77953ad33ae338d13b5d86b567c7b0
subjen <subbotinjenay@mail.ru>
2025-11-27 16:34:12 +0500

    Solve 714. Best Time to Buy and Sell Stock with Transaction Fee https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/submissions/1840969989/

---
+++ AlgorithmsSolver/Queue/DynamicProgramming/BestTimetoBuyandSellStockwithTransactionFeeSolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsSolver.Queue.DynamicProgramming
+{
+    //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/description/
+    internal class BestTimetoBuyandSellStockwithTransactionFeeSolver
+    {
+        public int MaxProfit(int[] prices, int fee)
+        {
+            var currMin = prices[0];
+            var max = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > currMin + fee)
+                {
+                    max += prices[i] - currMin - fee;
+                    currMin = prices[i] - fee;
+                }
+                currMin = Math.Min(currMin, prices[i]);
+            }
+            return max;
+        }
+    }
+}
